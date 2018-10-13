ffb36438f7099c2f1f3a59f406982ecb7b91cbdc
Simon Schmid <mail@sschmid.com>
2018-10-13 19:22:54 +0200

    Initial commit

---
+++ EntitasPure/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EntitasPure
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var contexts = Contexts.sharedInstance;
+            var e = contexts.game.CreateEntity();
+            e.AddHealth(100);
+
+            System.Console.WriteLine("e.health.value: " + e.health.value);
+        }
+    }
+}
